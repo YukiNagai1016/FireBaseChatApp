f6b9da948830cd74cc52a5e0a51a874b530dafa6
優樹永井 <wadachi0505@gmail.com>
2019-04-12 22:37:12 +0900

    Firebase

--- FireApp/ViewController.swift
+++ FireApp/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import FirebaseCore
+import Firebase
+import FirebaseDatabase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var messageField: UITextField!
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    var databaseRef:DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageField.delegate = self
+        
+        databaseRef = Database.database().reference()
+        // databaseを呼んでる？？
+        
+        databaseRef.observe(DataEventType.childAdded, with: { snapshot in //セッティングしている。新しいデータが入ってくるのを見張ってる。もしも、新しいデータが入ってきた場合{}内の処理を実行する。
+            if let name = (snapshot.value! as AnyObject).object(forKey: "name") as? String,
+                let message = (snapshot.value! as AnyObject).object(forKey: "message") as? String {
+                self.textView.text! = "\(self.textView.text!)\n\(name) : \(message)"
+                
+            }
+        })
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        
+        let messageData = ["name": nameField.text!, "message": messageField.text!]
+        // firebaseにdictionary型を保存するためデータを作成！
+        
+        databaseRef.childByAutoId().setValue(messageData) // データを保存！
+        
+        textField.resignFirstResponder() //キーボードを閉じる
+        messageField.text = "" //最終的にはtextFieldを空にする。
+        
+        return true
+    }
 }
 
